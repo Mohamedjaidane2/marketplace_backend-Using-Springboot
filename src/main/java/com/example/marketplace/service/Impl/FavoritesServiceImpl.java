8da19a53d94e7e8a0ff7a9ddc35a31cf2e437ba6
Abdallah-Raies <raiesabdallah@gmail.com>
2023-08-22 20:54:12 +0200

    Feat: setup service implementation

---
+++ src/main/java/com/example/marketplace/service/Impl/FavoritesServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.marketplace.service.Impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FavoritesServiceImpl implements IFavoritesService{
+}
---
+++ src/main/java/com/example/marketplace/service/Impl/HistoryServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.marketplace.service.Impl;
+
+public class HistoryServiceImpl implements IHistoryService{
+}
